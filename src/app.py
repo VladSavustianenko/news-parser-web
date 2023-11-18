6e848f3ecfaf0b946310857f5e29fe80a003be6d
VladSavustianenko <vladsavustianenko@gmail.com>
2023-11-18 20:44:15 +0200

    fix db config

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:admin' \
 #                                                        f'@localhost/news'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://vezxqcorwfukqk:a5992eddeaeec73a3030c38c124e21f84b725391fab70658e2a444377c132640@ec2-18-202-8-133.eu-west-1.compute.amazonaws.com:5432/dfqcc1kqh52rno'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vezxqcorwfukqk:a5992eddeaeec73a3030c38c124e21f84b725391fab70658e2a444377c132640@ec2-18-202-8-133.eu-west-1.compute.amazonaws.com:5432/dfqcc1kqh52rno'
 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
