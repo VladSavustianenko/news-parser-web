da5cd047b627e1937b21af2ab449a87b74e0a3a0
VladSavustianenko <vladsavustianenko@gmail.com>
2023-11-18 20:54:33 +0200

    show errors

--- src/parser.py
+++ src/parser.py
@@ -29,83 +29,83 @@ class Parser:
             'query': '{"query": "prism://prism.query' + query + '&offset=' + str(offset) + '&limit=' + str(self._LIMIT) + '"}'
         }
 
-        try:
-            resp = requests.get(self._URL, params)
-            json_data = resp.json()
-            items = json_data['items']
+        # try:
+        resp = requests.get(self._URL, params)
+        json_data = resp.json()
+        items = json_data['items']
 
-            repeats = 0
-            for item in items:
-                if not self._is_already_exist(item['_id']):
-                    repeats = 0
-                    topic_model = TopicModel()
+        # repeats = 0
+        for item in items:
+            if not self._is_already_exist(item['_id']):
+                repeats = 0
+                topic_model = TopicModel()
 
-                    topic_model.section = section
-                    topic_model.subsection = subsection
+                topic_model.section = section
+                topic_model.subsection = subsection
 
-                    topic_model.source_id = item['_id']
-                    topic_model.type = item['type']
+                topic_model.source_id = item['_id']
+                topic_model.type = item['type']
 
-                    if 'tracking' in item and 'author_name' in item['tracking']:
-                        topic_model.author = item['tracking']['author_name']
+                if 'tracking' in item and 'author_name' in item['tracking']:
+                    topic_model.author = item['tracking']['author_name']
 
-                    if 'headlines' in item and 'basic' in item['headlines']:
-                        topic_model.headline = item['headlines']['basic']
+                if 'headlines' in item and 'basic' in item['headlines']:
+                    topic_model.headline = item['headlines']['basic']
 
-                    if 'website_url' in item:
-                        topic_model.website_url = item['website_url']
+                if 'website_url' in item:
+                    topic_model.website_url = item['website_url']
 
-                    if 'fusion_additions' in item and 'teaser' in item['fusion_additions']:
-                        topic_model.teaser = item['fusion_additions']['teaser']
+                if 'fusion_additions' in item and 'teaser' in item['fusion_additions']:
+                    topic_model.teaser = item['fusion_additions']['teaser']
 
-                    if 'fusion_additions' in item and 'promo_image' in item['fusion_additions'] and 'url' in item['fusion_additions']['promo_image']:
-                        topic_model.promo_image = item['fusion_additions']['promo_image']['url']
+                if 'fusion_additions' in item and 'promo_image' in item['fusion_additions'] and 'url' in item['fusion_additions']['promo_image']:
+                    topic_model.promo_image = item['fusion_additions']['promo_image']['url']
 
-                    if 'first_publish_date' in item:
-                        topic_model.publish_date = item['first_publish_date']
+                if 'first_publish_date' in item:
+                    topic_model.publish_date = item['first_publish_date']
 
-                    if 'description' in item and 'basic' in item['description']:
-                        topic_model.description = item['description']['basic']
+                if 'description' in item and 'basic' in item['description']:
+                    topic_model.description = item['description']['basic']
 
-                    new_topic = Topic(topic_model)
+                new_topic = Topic(topic_model)
 
-                    db.session.add(new_topic)
+                db.session.add(new_topic)
+                db.session.commit()
+                print('Add topic id:', new_topic.source_id, ' --- ', new_topic.headline.encode("utf-8"))
+
+                content_list = []
+                if 'content_elements' in item and len(item['content_elements']):
+                    order = 1
+                    for elem in item['content_elements']:
+                        content_model = ContentModel()
+                        if 'content' in elem and '_id' in elem:
+                            content_model.topic_id = new_topic.id
+                            content_model.source_id = elem['_id']
+                            content_model.content = elem['content']
+                            content_model.order = order
+
+                            content_list.append(content_model)
+
+                            order += 1
+                    entities = [Content(content) for content in content_list]
+                    db.session.add_all(entities)
                     db.session.commit()
-                    print('Add topic id:', new_topic.source_id, ' --- ', new_topic.headline.encode("utf-8"))
 
-                    content_list = []
-                    if 'content_elements' in item and len(item['content_elements']):
-                        order = 1
-                        for elem in item['content_elements']:
-                            content_model = ContentModel()
-                            if 'content' in elem and '_id' in elem:
-                                content_model.topic_id = new_topic.id
-                                content_model.source_id = elem['_id']
-                                content_model.content = elem['content']
-                                content_model.order = order
+                new_topic_log = TopicLog(new_topic.id)
 
-                                content_list.append(content_model)
+                db.session.add(new_topic_log)
+                db.session.commit()
+            else:
+                print(item['_id'], 'Already exist')
+                # repeats += 1
 
-                                order += 1
-                        entities = [Content(content) for content in content_list]
-                        db.session.add_all(entities)
-                        db.session.commit()
-
-                    new_topic_log = TopicLog(new_topic.id)
-
-                    db.session.add(new_topic_log)
-                    db.session.commit()
-                else:
-                    print(item['_id'], 'Already exist')
-                    repeats += 1
-
-                    if repeats == 5:
-                        print('\nBREAK DUE TO 5 REPEATS\n')
-
-                        return
-        except:
-            print('\nCOULD NOT FETCH DATA', params)
-            return
+                # if repeats == 5:
+                #     print('\nBREAK DUE TO 5 REPEATS\n')
+                #
+                #     return
+        # except:
+        #     print('\nCOULD NOT FETCH DATA', params)
+        #     return
 
     def execute(self):
         with app.app_context():
