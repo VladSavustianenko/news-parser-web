21c5e72717841de5f551d137d91e3c3fe80712ca
VladSavustianenko <vladsavustianenko@gmail.com>
2024-05-06 17:45:37 +0300

    update database creds

--- src/app.py
+++ src/app.py
@@ -5,9 +5,9 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:admin' \
-                                                       f'@localhost/news'
-# app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.getenv("DATABASE_USER")}:{os.getenv("DATABASE_PASSWORD")}@{os.getenv("DATABASE_HOST")}:{os.getenv("DATABASE_PORT")}/{os.getenv("DATABASE_NAME")}'
+# app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:admin' \
+#                                                        f'@localhost/news'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.getenv("DATABASE_USER")}:{os.getenv("DATABASE_PASSWORD")}@{os.getenv("DATABASE_HOST")}:{os.getenv("DATABASE_PORT")}/{os.getenv("DATABASE_NAME")}'
 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
